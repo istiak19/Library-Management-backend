fb5da6b1c695bf058fea3a27ce7d888810c8aa74
istiak19 <istiak15-14128@diu.edu.bd>
2025-07-01 18:36:45 +0600

    cors install

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import cors from "cors";
 import express, { Application, Request, Response } from "express";
 import { bookRouter } from "./app/controllers/book.controller";
 import { borrowRoute } from "./app/controllers/borrow.controller";
@@ -5,12 +6,19 @@ import { borrowRoute } from "./app/controllers/borrow.controller";
 const app: Application = express();
 
 app.use(express.json());
+app.use(cors({
+    origin: [
+        "http://localhost:5173",
+        "http://localhost:5174"
+    ],
+    credentials: true
+}));
 
 app.use("/api", bookRouter);
 app.use("/api", borrowRoute);
 
 app.get("/", (req: Request, res: Response) => {
-    res.send("Welcome to Library Management!");
+    res.send("Library Management System API is running successfully!");
 });
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -10,9 +10,9 @@ let server: Server;
 async function main() {
     try {
         await mongoose.connect(process.env.MONGO_URI as string);
-        console.log("Connection to MongoDB!")
+        console.log("Connected to MongoDB!")
         server = app.listen(port, () => {
-            console.log(`Library Management app listening on port ${port}`)
+            console.log(`Server is running at http://localhost:${port}`)
         })
     } catch (error) {
         console.log(error)
